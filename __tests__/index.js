04795ea13a8dac4aa822616306eb27b42ec63ab9
Zweer <niccolo@olivieriachille.com>
2017-02-14 10:59:07 +0100

    Added all the urls to test

--- __tests__/index.js
+++ __tests__/index.js
@@ -3,25 +3,25 @@ import { sequelize, Feed, Article } from 'mtg-omega-models/sql';
 import { batch } from '../src';
 
 describe('Feed Importer', () => {
-  const url1 = 'http://www.gatheringmagic.com/feed/';
-
   beforeEach(() => sequelize.sync({ force: true }));
 
-  it('should store articles from site1', () => Feed.create({ url: url1 })
-    .then(() => batch())
-    .then(() => Promise.all([
-      Feed.findAll({ include: [{ model: Article }] }),
-      Article.findAll(),
-    ]))
-    .then(([feeds, articles]) => {
-      expect(feeds).toHaveLength(1);
-      expect(articles).not.toHaveLength(0);
+  [
+    'http://www.gatheringmagic.com/feed/',
+    'http://magic.tcgplayer.com/rss/rssfeed.xml',
+    'http://www.eternalcentral.com/feed/',
+    'http://www.mtgsalvation.com/articles.rss',
+  ].forEach((url) => {
+    it(`should store articles from "${url}"`, () => Feed.create({ url })
+      .then(() => batch())
+      .then(() => Promise.all([
+        Feed.findAll({ include: [{ model: Article }] }),
+        Article.findAll(),
+      ]))
+      .then(([feeds, articles]) => {
+        expect(feeds).toHaveLength(1);
+        expect(articles).not.toHaveLength(0);
 
-      expect(feeds[0].articles.length).toBe(articles.length);
-    })
-    .catch((err) => {
-      console.error(err);
-
-      throw new Error(err);
-    }));
+        expect(feeds[0].articles.length).toBe(articles.length);
+      }));
+  });
 });
